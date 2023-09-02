96b1093fe6763ede5c0a9f4ad55dab3d93f84f5c
Kelwinpin <kelwincomw@gmail.com>
2023-09-02 18:48:21 -0300

    add sanitize function

--- src/robots/text.js
+++ src/robots/text.js
@@ -7,13 +7,31 @@ function textRobot(content) {
     async function fetchContentFromWikipedia(content) {
 
         try {
-            await wiki.setLang('en');
+            await wiki.setLang('pt');
             const page = await wiki.page(`${content.searchTerm}`)
-            const summary = await page.summary();
-            console.log(chalk.greenBright(summary.extract))
+            content.originalSource = await page.content();
+            sanitizeContent(content.originalSource)
         } catch (error) {
             console.log(chalk.redBright(error.message));
         }
+    }
+
+    function sanitizeContent(text) {
+        const sanitizeText = withoutBlankLinesAndMarkdown(text);
+        console.log(sanitizeText);
+
+        function withoutBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n');
+            const withoutBlankLinesAndMarkdown = allLines.filter(line => {
+                if (line.trim().length === 0 || line.trim().startsWith("=")) {
+                    return false;
+                }
+                return true;
+            })
+            return withoutBlankLinesAndMarkdown.join(" ");
+        }
+
+
     }
 }
 
